36e551bd4aaa79418bf2c785594cc465388992b1
youstina <youstina@youstina.(none)>
2014-02-12 13:59:07 +0200

    WIP

--- db/migrate/20140209081840_create_sync_uuids.rb
+++ db/migrate/20140209081840_create_sync_uuids.rb
@@ -1,18 +1,13 @@
-class CreateSyncLogActionParameters < ActiveRecord::Migration
+class CreateSyncUuids < ActiveRecord::Migration
   def up
-    create_table :sync_log_action_parameters do |t|
-      t.integer :peer_log_id
-      t.integer :param_object_type_id
-      t.integer :param_object_id
-      t.integer :param_object_site_id
-      t.string :parameter
-      t.string :value
+    create_table :sync_uuids do |t|
+      t.string :current_uuid
 
       t.timestamps
     end
   end
 
   def down
-    drop_table :sync_log_action_parameters
+    drop_table :sync_uuids
   end
 end
