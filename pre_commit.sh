9298d745900eef2514d3301a8739cc2826405c84
youstina <youstina.atef@bibalex.org>
2014-08-03 10:46:57 +0200

    last pre-commit file as a reminder to remove any debugger references

--- pre_commit.sh
+++ pre_commit.sh
@@ -1,6 +1,4 @@
-FILES_PATTERN='\.(js|coffee)(\..+)?$'
-FORBIDDEN='console.log'
-git diff --cached --name-only | \
-    grep -E $FILES_PATTERN | \
-    GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -n $FORBIDDEN && echo 'COMMIT REJECTED Found "$FORBIDDEN" references. Please remove them before commiting' && exit 1
-
+FILES_PATTERN='*.rb'
+FORBIDDEN='debugger'
+find . -name $FILES_PATTERN | \
+    GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -n $FORBIDDEN && echo 'COMMIT REJECTED Found "debugger" references. Please remove them before commiting OR use --no-verify option for git commit' && exit 1
