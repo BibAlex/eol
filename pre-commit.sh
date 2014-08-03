387c2f44b1466be37932715a039c8bb48c27392c
youstina <youstina.atef@bibalex.org>
2014-08-03 10:12:59 +0200

    test debugger

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,5 +1,5 @@
 FILES_PATTERN='\.(rb)'
-FORBIDDEN='console.log'
+FORBIDDEN='search_suggestion_controller.rb'
 git diff --cached --name-only | \
     grep -E $FILES_PATTERN | \
     GREP_COLOR='4;5;37;41' xargs grep --color --with-filename -n $FORBIDDEN && echo 'COMMIT REJECTED Found "$FORBIDDEN" references. Please remove them before commiting' && exit 1
